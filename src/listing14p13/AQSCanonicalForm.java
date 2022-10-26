a4e5754adc5f7ccfa7a7afbb73891487a24a750d
chlau6 <lauchiho970731@gmail.com>
2022-10-26 20:57:53 +0800

    comment

--- src/listing14p13/AQSCanonicalForm.java
+++ src/listing14p13/AQSCanonicalForm.java
@@ -33,15 +33,20 @@ public class AQSCanonicalForm {
 }
 
 /*
-The interfaces of ReentrantLock and Semaphore have a lot in common. Both classes act as a "gate",
-allowing only a limited number of threads to pass at a time; threads arrive at the gate and are allowed through
-(lock or acquire returns successfully), are made to wait (lock or acquire blocks), or are turned away
-(tryLock or tryAcquire returns false, indicating that the lock or permit did not become available in the time allowed).
-Further, both allow interruptible, uninterruptible,
-and timed acquisition attempts, and both allow a choice of fair or nonfair queueing of waiting threads.
+Acquisition and release in AQS take the forms shown in Listing 14.13. Depending on the synchronizer,
+acquisition might be exclusive, as with Reentrant- Lock, or nonexclusive, as with Semaphore and CountDownLatch.
 
-Given this commonality, you might think that Semaphore was implemented on top of ReentrantLock,
-or perhaps ReentrantLock was implemented as a Semaphore with one permit. This would be entirely practical;
-it is a common exercise to prove that a counting semaphore can be implemented using a lock
-(as in SemaphoreOnLock in Listing 14.12) and that a lock can be implemented using a counting semaphore.
+An acquire operation has two parts. First, the synchronizer decides whether the current state permits acquisition;
+if so, the thread is allowed to proceed, and if not, the acquire blocks or fails.
+
+This decision is determined by the synchronizer semantics; for example,
+acquiring a lock can succeed if the lock is unheld,
+and acquiring a latch can succeed if the latch is in its terminal state.
+
+The second part involves possibly updating the synchronizer state; 
+one thread acquiring the synchronizer can affect whether other threads can acquire it. 
+For example, acquiring a lock changes the lock state from "unheld" to "held", 
+and acquiring a permit from a Semaphore reduces the number of permits left. 
+On the other hand, the acquisition of a latch by one thread does not affect whether other threads can acquire it, 
+so acquiring a latch does not change its state.
 */
